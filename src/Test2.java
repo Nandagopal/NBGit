a14f2601d52a99b1f6b532360768206811646fd0
Nanda Gopal <nandagopal.y@myiton.com>
2013-01-10 14:23:14 +0530

    commiting from trail

--- src/Test2.java
+++ src/Test2.java
@@ -4,7 +4,8 @@ public static void main(string args[]){
 
 System.out.println("hello iton");
 System.out.println("hello iton 123");
-System.out.pringln("hello from iton 12345");
+System.out.println("hello from iton 12345");
+System.out.println("hello from iton 54678");
 }
 
 }
